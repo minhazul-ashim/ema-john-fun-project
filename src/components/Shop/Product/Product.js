17c92355dbef69dbfafa43edd7b6a8fc79019b5d
Md.Minhazul Haque <ashim.ashim7470@gmail.com>
2021-09-24 21:21:05 +0600

    some data are managed

--- src/components/Shop/Product/Product.js
+++ src/components/Shop/Product/Product.js
@@ -1,10 +1,20 @@
 import './Product.css'
 import React from 'react';
 
-const Product = () => {
+const Product = (props) => {
+    console.log(props);
+    const { name, img, seller, price, stock } = props.data;
     return (
-        <div>
-            
+        <div className='product-box'>
+            <div className="product-image">
+                <img src={img} alt="" />
+            </div>
+            <div className="product-details">
+                <h3>{name}</h3>
+                <p><small>By {seller}</small></p>
+                <p><small>${price}</small></p>
+                <p><small>Stock Left: {stock}</small></p>
+            </div>
         </div>
     );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,5 +1,6 @@
 import './Shop.css'
 import React, { useEffect, useState } from 'react';
+import Product from './Product/Product';
 
 const Shop = () => {
 
@@ -13,10 +14,16 @@ const Shop = () => {
     }, [])
 
     return (
-        <div class='shop-container'>
+        <div className='shop-container'>
             <div className="products-container">
                 <h2>Total Products : {products.length}</h2>
-                
+
+                {
+                    products.map(product => {
+
+                        return <Product key={product.key} data={product}></Product>
+                    })
+                }
             </div>
             <div className="cart-info">
                 <h2>My Cart</h2>
